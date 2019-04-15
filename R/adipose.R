35608b4ed04783a97a6aca91930113471be74050
kwanjeeraw <kwanjeeraw@gmail.com>
2019-04-15 19:29:16 +0700

    modify gh-branch

--- R/adipose.R
+++
@@ -1,9 +0,0 @@
-#' Adipose tissue data
-#' 
-#' Metabolomics data of adipose tissue samples
-#' @format A data frame with 118 rows and 167 columns:
-#' \describe{
-#' ...
-#' }
-#' @source \url{https://www.metabolomicsworkbench.org/}
-"adipose"
